76593ac96a0a34c288a33d05f738b2b0f16dc71b
Tyler <tylerk516@gmail.com>
2024-04-10 22:06:12 -0700

    updated tsconfi

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lusitana, Montserrat, Playfair_Display } from 'next/font/google';
+
+export const playfair = Playfair_Display({weight: "500", subsets: ['latin'] });
+export const montserrat = Montserrat({weight: "500", subsets: ['latin'] });
+export const lusitana = Lusitana({weight: "400", subsets: ['latin'] });
